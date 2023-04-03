ea07cc2e94a6feaa2f74a98c61b67449a51dae85
marovichn <nikolamarovic.11@gmail.com>
2023-04-03 21:46:40 +0200

    managing-loading-state

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -2,15 +2,21 @@ import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 import classes from "./AvailableMeals.module.css";
 import { useEffect, useState } from "react";
+import React from "react";
 
 const AvailableMeals = () => {
   const [mealsData, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
 
   useEffect(() => {
     const FetchMeals = async () => {
       const response = await fetch(
         "https://simple-react-app-2b7b6-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
       );
+      if (!response.ok) {
+        return;
+      }
       const responseData = await response.json();
       const loadedMeals = [];
       for (const key in responseData) {
@@ -23,9 +29,18 @@ const AvailableMeals = () => {
         loadedMeals.push(dataObject);
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     FetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.mealsLoading}>
+        <h1>Loading...</h1>
+      </section>
+    );
+  }
 
   const mealsList = mealsData.map((meal) => (
     <MealItem
